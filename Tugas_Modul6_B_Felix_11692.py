c7bec969ead638e9acfc2b1c6f33432507b70605
FelixTanaka <220711692@students.uajy.ac.id>
2024-12-04 15:27:21 +0700

    Update Tugas_Modul6_B_Felix_11692.py

--- Tugas_Modul6_B_Felix_11692.py
+++ Tugas_Modul6_B_Felix_11692.py
@@ -6,7 +6,7 @@ from PIL import Image
 
 # Load the pre-trained model
 # (Sesuai dengan path model Anda (model terbaik yang di dump dalam format .h5))
-model = load_model(r'C:\Users\LENOVO\Documents\Semester 5\Pembelajaran mesin dan mendalam\Bagian 6\Tugas6_B_11692\model_mobilenet.h5')
+model = C:\Users\LENOVO\Documents\Semester 5\Pembelajaran mesin dan mendalam\Bagian 6\Tugas6_B_11692\model_mobilenet.h5
 class_names = ["Matang", "Mentah"]
 
 # Function to preprocess and classify image
